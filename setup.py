a086e2c22278eef2af08eaa68284bf27b2525a8c
Brydon Leonard <brydon.leonard@gmail.com>
2023-09-12 23:59:47 +0200

    Fix URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='cloudflare_ddns',
       version='0.1',
       description='Simple script to make CloudFlare DNS dynamic!',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/BrydonLeonard/CloudFlareDDNS',
       author='Brydon Leonard',
       author_email='brydon.leonard@gmail.com',
       license='MIT',
